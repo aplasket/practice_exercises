be826e243232f3c77517b5f4411c0b37ca909524
Ashley Plasket <ashley.plasket@gmail.com>
2023-03-24 11:13:23 -0400

    def method to capitialize every other word in string and return array

--- lib/1practice.rb
+++ lib/1practice.rb
@@ -23,15 +23,46 @@
 # ---------------- 
 # Practice 2:
 
-prices = ["$2.50", 
-          "32.25$",
-          "3$.25",
-          "9.$50",
-          "75.98"]
-refactored = []
+# prices = ["$2.50", 
+#           "32.25$",
+#           "3$.25",
+#           "9.$50",
+#           "75.98"]
+# refactored = []
 
- refactored = prices.map do |price|
-    price.delete! "$"
-    "$%05.2f" % price
+#  refactored = prices.map do |price|
+#     price.delete! "$"
+#     "$%05.2f" % price
+#   end
+
+
+#Sentence Array: Write a method that adds each word of a sentence to an array. 
+#Then, capitalize every other word in the sentence. Return the new sentence.
+
+#- Example:
+    
+ #   "The quick brown fox jumps over the lazy dog." ["The", "quick", "brown" …]
+    
+  #  => "The quick Brown fox Jumps over The lazy Dog."
+   
+# - Extensions:
+#     - Use a sentence with other capital letters in it. The method should still capitalize every alternate word, regardless of other capitalization.
+#     - Use a sentence with special characters, spaces, etc. Does your method still work?
+#     - Refactor your solution to keep your main method under 5 lines. Use helper methods.
+#     - Write a test suite to prove that your solution works.
+
+def capitalize_every_other_word
+  sentence_array = sentence.split(' ')
+  sentence_array.each do |word|
+    if sentence_array.index(word) % 2 == 0
+      word.capitalize!
+    end
   end
+  transformed = sentence_array.join(" ")
+end
 
+#perhaps try with index(0) or index.even? if index % 2 == 0, word.upcase, end
+
+# def capitalize_words(string)
+#  sentence.split.map(&:capitalize).join(' ')
+# end
---
+++ spec/1practice_spec.rb
@@ -0,0 +1,25 @@
+require 'rspec'
+require './lib/1practice.rb'
+
+ RSpec.describe '1practice' do
+#   it "tests" do
+#     prices = ["$2.50", 
+#           "32.25$",
+#           "3$.25",
+#           "9.$50",
+#           "75.98"]
+#     refactored = []
+
+#     prices.dollar_sign_remove
+#   end
+
+  it "splits_string" do
+    sentence = "hi, happy first week of coding!"
+    sentence_array = []
+    transformed = []
+
+    split_string(sentence)
+require 'pry'; binding.pry
+    expect(sentence.captilaize_words).to eq(["Hi,", "happy", "First", "week", "Of", "coding!"])
+  end
+end
